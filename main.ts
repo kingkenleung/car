41d41764f2f2bd743bf6049d1833579b63ea61ed
kingkenleung <30319344+kingkenleung@users.noreply.github.com>
2022-10-12 17:17:19 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,58 @@
-function move_y(speed: number) {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, speed)
-    SuperBit.MotorRun(SuperBit.enMotors.M2, speed)
-    SuperBit.MotorRun(SuperBit.enMotors.M3, speed)
-    SuperBit.MotorRun(SuperBit.enMotors.M4, speed)
+
+function push(speed: number, turnIndex: number) {
+    // speed: -255 to 255
+    // turnIndex: -1 to 1
+    torgue = 100
+    if (turnIndex <= -0.15) {
+        // front-left
+        SuperBit.MotorRun(SuperBit.enMotors.M1, speed + torgue * Math.abs(turnIndex))
+        // hind-left
+        SuperBit.MotorRun(SuperBit.enMotors.M2, speed + torgue * Math.abs(turnIndex))
+        // front-right
+        SuperBit.MotorRun(SuperBit.enMotors.M3, speed - torgue * Math.abs(turnIndex))
+        // hind-right
+        SuperBit.MotorRun(SuperBit.enMotors.M4, speed - torgue * Math.abs(turnIndex))
+    } else if (turnIndex >= 0.15) {
+        // front-left
+        SuperBit.MotorRun(SuperBit.enMotors.M1, speed - torgue * Math.abs(turnIndex))
+        // hind-left
+        SuperBit.MotorRun(SuperBit.enMotors.M2, speed - torgue * Math.abs(turnIndex))
+        // front-right
+        SuperBit.MotorRun(SuperBit.enMotors.M3, speed + torgue * Math.abs(turnIndex))
+        // hind-right
+        SuperBit.MotorRun(SuperBit.enMotors.M4, speed + torgue * Math.abs(turnIndex))
+    } else {
+        // front-left
+        SuperBit.MotorRun(SuperBit.enMotors.M1, speed)
+        // hind-left
+        SuperBit.MotorRun(SuperBit.enMotors.M2, speed)
+        // front-right
+        SuperBit.MotorRun(SuperBit.enMotors.M3, speed)
+        // hind-right
+        SuperBit.MotorRun(SuperBit.enMotors.M4, speed)
+    }
 }
 function shift_left() {
-    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
-    SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
-    SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
-    SuperBit.MotorRun(SuperBit.enMotors.M4, -255)
-}
-function shift_right() {
     SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
     SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
     SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
     SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
 }
+function shift_right() {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+    SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M4, -255)
+}
 radio.onReceivedValue(function (name, value) {
     mapValueToVar(name, value)
-    if (left && !right) {
+    if (left && !(right)) {
         shift_left()
-    } else if (!left && right) {
+    } else if (!(left) && right) {
         shift_right()
     } else {
-        move_y(angleToSpeed(y))
+        push((y + 90) / 180 * 255, (x + 40) / 180)
     }
-
 })
 function mapValueToVar(key: string, value: number) {
     if (key == "x") {
@@ -38,11 +65,9 @@ function mapValueToVar(key: string, value: number) {
         right = value
     }
 }
-function angleToSpeed(angle: number) {
-    return angle / 180 * 255
-}
-let right = 0
-let left = 0
 let x = 0
 let y = 0
+let right = 0
+let left = 0
+let torgue = 0
 radio.setGroup(1)
