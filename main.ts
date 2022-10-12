36d4f593fbbbb72fb42b6565d0cc05310f1c40ae
kingkenleung <30319344+kingkenleung@users.noreply.github.com>
2022-10-12 16:44:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,48 @@
-let x = 0;
-let y = 0;
-let left = 0;
-let right = 0;
+function move_y(speed: number) {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, speed)
+    SuperBit.MotorRun(SuperBit.enMotors.M2, speed)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, speed)
+    SuperBit.MotorRun(SuperBit.enMotors.M4, speed)
+}
+function shift_left() {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, -255)
+    SuperBit.MotorRun(SuperBit.enMotors.M2, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M4, -255)
+}
+function shift_right() {
+    SuperBit.MotorRun(SuperBit.enMotors.M1, 255)
+    SuperBit.MotorRun(SuperBit.enMotors.M2, -255)
+    SuperBit.MotorRun(SuperBit.enMotors.M3, -255)
+    SuperBit.MotorRun(SuperBit.enMotors.M4, 255)
+}
+radio.onReceivedValue(function (name, value) {
+    mapValueToVar(name, value)
+    if (left && !right) {
+        shift_left()
+    } else if (!left && right) {
+        shift_right()
+    } else {
+        move_y(angleToSpeed(y))
+    }
 
-
-basic.forever(function () {
-	
 })
-
-radio.onReceivedValue((name: string, value: number) => {
-    
-})
-
+function mapValueToVar(key: string, value: number) {
+    if (key == "x") {
+        x = value
+    } else if (key == "y") {
+        y = value
+    } else if (key == "left") {
+        left = value
+    } else if (key == "right") {
+        right = value
+    }
+}
+function angleToSpeed(angle: number) {
+    return angle / 180 * 255
+}
+let right = 0
+let left = 0
+let x = 0
+let y = 0
+radio.setGroup(1)
